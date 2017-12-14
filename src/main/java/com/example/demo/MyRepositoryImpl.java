3b7c5e1bcd90d1408d93f0d0e7fdbee5c6a5af8d
Andreas Neiser <andreas.neiser@gmail.com>
2017-12-14 06:37:44 +0100

    Illustrate problem with @SpyBean and @Caching

--- src/main/java/com/example/demo/MyRepositoryImpl.java
+++ src/main/java/com/example/demo/MyRepositoryImpl.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
@@ -8,18 +10,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Repository
-class MyRepositoryImpl implements MyRepository {
+public class MyRepositoryImpl implements MyRepository {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(MyRepositoryImpl.class);
 
 	private Map<String, String> storage = new HashMap<>();
 
 	@Override
-	@Cacheable("items")
+	@Cacheable(cacheNames = "items", key = "#key.id")
 	public String getItem(String key) {
+		LOGGER.info("Storage get {}", key);
 		return storage.get(key);
 	}
 
 	@Override
-	@CacheEvict(cacheNames = "items", key = "#key")
+	@CacheEvict(cacheNames = "items", key = "#key.id")
 	public void putItem(String key, String value) {
 		storage.put(key, value);
 	}
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,52 @@
 package com.example.demo;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
+	// the following fails with a MockitoPostProcessor#inject exception
+	// Bean named 'myRepositoryImpl' is expected to be of type 'com.example.demo.MyRepositoryImpl'
+	// but was actually of type 'com.example.demo.$Proxy54'
+
+	// @SpyBean
+	// private MyRepositoryImpl sut;
+
+
+	// the following works as expected: the key is only retrieve once
+	// (verified by looking at the logging output when running the test)
+	// but no Mockito.verify is possible on such an object
+
+	// @Autowired
+	// private MyRepositoryImpl sut;
+
+
+	// the ideal, but non-working solution
+	@SpyBean
+	private MyRepository sut;
+
+
 	@Test
-	public void contextLoads() {
+	public void getTwice() {
+		sut.putItem("key1", "value1");
+		// retrieve key twice
+		assertThat(sut.getItem("key1")).isEqualTo("value1");
+		assertThat(sut.getItem("key1")).isEqualTo("value1");
+		// due to caching, the repository method should have been called once
+		// but this verify fails with TooManyActualInvocations!
+		// somehow, the caching abstraction was removed by @SpyBean?!
+		verify(sut, times(1)).getItem("key1");
 	}
 
 }
